14aa5bc8cb457774c8864c8e8d73be4df6ab189f
SofiaMorenoPerez <smorenoper@unbosque.edu.co>
2026-05-09 17:56:35 -0500

    estructura inicial backend Spring Boot

---
+++ centroadoptivo/centroadoptivo/src/main/java/co/edu/unbosque/centroadoptivo/CentroadoptivoApplication.java
@@ -0,0 +1,13 @@
+package co.edu.unbosque.centroadoptivo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentroadoptivoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CentroadoptivoApplication.class, args);
+	}
+
+}
---
+++ centroadoptivo/centroadoptivo/src/test/java/co/edu/unbosque/centroadoptivo/CentroadoptivoApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.unbosque.centroadoptivo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentroadoptivoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
