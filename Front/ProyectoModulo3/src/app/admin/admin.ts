c25779f3569706337b0cb042e92e0c9a8858b4f1
imoreno1010 <imorenope@unbosque.edu.co>
2026-05-21 09:32:22 -0500

    Front-end admin
    The admin front-end is finished

--- Front/ProyectoModulo3/src/app/admin/admin.ts
+++ Front/ProyectoModulo3/src/app/admin/admin.ts
@@ -1,9 +1,24 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-admin',
   standalone: false,
   templateUrl: './admin.html',
-  styleUrl: './admin.css',
+  styleUrl: './admin.css'
 })
-export class Admin {}
+export class Admin {
+  vista: string = 'transacciones';
+  menuAbierto: boolean = false;
+
+  constructor(private router: Router) {}
+
+  cambiarVista(v: string) {
+    this.vista = v;
+    this.menuAbierto = false;
+  }
+
+  cerrarSesion() {
+    this.router.navigate(['/inicio']);
+  }
+}
