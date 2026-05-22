dc130eec45f8b51b12cf5966d9920c467e1eb07c
imoreno1010 <imorenope@unbosque.edu.co>
2026-05-22 07:38:58 -0500

    changes
    Change the port and add a @CrossOrigin

--- centroadoptivo/centroadoptivo/src/main/java/co/edu/unbosque/centroadoptivo/controller/AuthController.java
+++ centroadoptivo/centroadoptivo/src/main/java/co/edu/unbosque/centroadoptivo/controller/AuthController.java
@@ -7,6 +7,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,6 +23,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
 @RequestMapping("/auth")
+@CrossOrigin(origins = { "http://localhost:8081", "http://localhost:4200" })
 @Tag(name = "Autenticación", description = "API para autenticación de usuarios (login y registro)")
 public class AuthController {
 
